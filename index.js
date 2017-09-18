996030fa11447543f50608f786e5f6eb47f03b6d
Bitterulf <bitterulf@users.noreply.github.com>
2017-09-18 20:36:18 +0200

    Add simple auth

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ require('dotenv').config();
 const Joi = require('joi');
 
 const envSchema = Joi.object({
-    BUNKER_PORT: Joi.number().integer().required()
+    BUNKER_PORT: Joi.number().integer().required(),
+    BUNKER_ADMIN_USER: Joi.string().alphanum().required(),
+    BUNKER_ADMIN_PASSWORD: Joi.string().required()
 }).unknown(true);
 
 const envCheck = Joi.validate(process.env, envSchema);
@@ -24,28 +26,38 @@ const Hapi = require('hapi');
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({
-    port: process.env.BUNKER_PORT || 8080
+    port: process.env.BUNKER_PORT
 });
 
-// Add the route
-server.route({
-    method: 'GET',
-    path:'/hello',
-    handler: function (request, reply) {
-        db.insert([{ random: Math.random() }], function (err, newDocs) {
-            db.find({}, function (err, docs) {
-                return reply(JSON.stringify(docs));
+server.register(require('hapi-auth-basic'), (err) => {
+    server.auth.strategy('simple', 'basic', { validateFunc: function(request, username, password, callback) {
+        if (username != process.env.BUNKER_ADMIN_USER || password != process.env.BUNKER_ADMIN_PASSWORD) {
+            return callback(null, false, {});
+        }
+        callback(null, true, { username: process.env.BUNKER_ADMIN_USER });
+    } });
+
+    // Add the route
+    server.route({
+        method: 'GET',
+        path:'/hello',
+        config: { auth: 'simple' },
+        handler: function (request, reply) {
+            db.insert([{ random: Math.random() }], function (err, newDocs) {
+                db.find({}, function (err, docs) {
+                    return reply(JSON.stringify(docs));
+                });
             });
-        });
-    }
-});
+        }
+    });
 
-// Start the server
-server.start((err) => {
+    server.start((err) => {
 
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
-    console.log(process.memoryUsage());
+        if (err) {
+            throw err;
+        }
+        console.log('Server running at:', server.info.uri);
+        console.log(process.memoryUsage());
+    });
+
 });
